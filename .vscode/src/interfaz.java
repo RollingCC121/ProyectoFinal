3c875a4c598930ad4c4681b0b83ea1286fc81850
RollingCC121 <daniel.gaviria0605@gmail.com>
2021-05-08 18:28:49 -0500

    creando interfeaz

---
+++ .vscode/src/interfaz.java
@@ -0,0 +1,35 @@
+public class interfaz {
+
+     public static void main(String[] args) {
+
+    System.out.println("     ___________________________________________________________________    ");
+    System.out.println("   .'  _______________________________________________________________  '.  ");
+    System.out.println("   : .'                         | Bienvenidos |                       '. :  ");
+    System.out.println("   | |                          |      A      |                        | |  ");
+    System.out.println("   | |           UwU            |  All-Repro  |          UwU           | |  ");
+    System.out.println("   | |      ____________________|_____________|__________________      | |  ");
+    System.out.println("   | |    .:_____________________________________________________:.    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |                1.ver lista de canciones:              |    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |             2.ver la letra de una canción:            |    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |                3.reproducir una canción:              |    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |          4.reproducir una canción aleatoria:          |    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |                                                       |    | |  ");
+    System.out.println("   | |    |              __________________________               |    | |  ");
+    System.out.println("   | |    |             |  |  |  |  |  |  |  |  |  |              |    | |  ");
+    System.out.println("   | |    '.____________|__|__|__|__|__|__|__|__|__|____________.'     | |  ");
+    System.out.println("   | |                                                                 | |  ");
+    System.out.println("   | |                            ALL-Repro                            | |  ");
+    System.out.println("   : '._______________________________________________________________.' :  ");
+    System.out.println("    \".______________________________\\___/______________________________.\"  ");
+
+     }
+}
--- src/index.java
+++
@@ -1,6 +0,0 @@
-package src;
-public class index{
-    public static void main(String[] args){
-        
-    }
-}
